646c74e165fa2583d4c7445d43b2303628d6b25e
Guillermo Padron <g.padron@hylasoftusa.com>
2014-10-21 16:06:17 -0500

    Create StringExtension class file

---
+++ h-behavior/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hylasoft.Behavior.Extensions
+{
+  /// <summary>
+  /// Extensions to check Strings
+  /// </summary>
+  public static class StringExtensions
+  {
+     
+  }
+}
